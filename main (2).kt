33381342b872dd3c0eabdd31f882373b8039621b
jezrreloriginal <124934158+jezrreloriginal@users.noreply.github.com>
2023-05-31 16:16:06 -0300

    Add files via upload

---
+++ main (2).kt
@@ -0,0 +1,22 @@
+fun main() {
+  println("Hello world!")
+
+  fun main() {
+  println("olá Mundo")
+  var nome: String = "abner"
+  val nascimento: Int = 2005
+
+  println(nome)       
+  println(nascimento)   
+
+  val myNum: Int = 5            
+  val myLetter: Char = 'D'       
+  val myBoolean: Boolean = true      
+  val myText: String = "Olá"    
+
+  println (myNum)
+  println (myLetter)
+  println (myBoolean)
+  println (myText)
+}
+}
